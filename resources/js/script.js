c14cdf6da7dfac0be71b6ae3626e4660a9bba9d0
moodup <moodup@MacBook-Air-moodup.local>
2019-03-28 15:52:49 +0100

    add scrolling

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,4 +10,16 @@ $(document).ready(function () {
         offset: '60px;'
     });
 
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({
+            scrollTop: $('.js--section-plans').offset().top
+        }, 1000);
+    });
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.js--section-features').offset().top
+        }, 1000);
+    });
+
 });
